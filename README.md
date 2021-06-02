88128de173c59c29a6bada2179ccf6151c9e325b
likangjian <1480108535@qq.com>
2021-06-02 12:43:27 +0800

    添加月份下载功能

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 This project is made for my wife to count money
 
 # 2021.5.28 Start
+
+# 2021.6.2
++ 添加按月份下载功能
--- src/controller/show.js
+++ src/controller/show.js
@@ -1,7 +1,7 @@
 const Base = require('./base.js');
 
 module.exports = class extends Base {
-  
+
   async indexAction() {
     var myDate = new Date();
     let year = myDate.getFullYear();
@@ -16,17 +16,17 @@ module.exports = class extends Base {
     this.assign('data',data);
     let allarray = new Array()
     let i = 0;
-    for (i = 0; i < 2030; i++) { 
+    for (i = 0; i < 2030; i++) {
       allarray[i] = 0
-    } 
+    }
     let total = 0;
-    for (i = 0; i < data.length; i++) { 
+    for (i = 0; i < data.length; i++) {
       allarray[data[i].year] += data[i].money
       total += data[i].money
     }
-    for (i = 2021; i < 2030; i++) { 
+    for (i = 2021; i < 2030; i++) {
       allarray[i].toFixed(2);
-    } 
+    }
     //console.log(yeararray)
     this.assign('allarray',allarray);
     total = total.toFixed(2);
@@ -49,24 +49,24 @@ module.exports = class extends Base {
     this.assign('data',data);
     let yeararray = new Array()
     let i = 0;
-    for (i = 0; i < 13; i++) { 
+    for (i = 0; i < 13; i++) {
       yeararray[i] = 0
-    } 
+    }
     let total = 0;
-    for (i = 0; i < data.length; i++) { 
+    for (i = 0; i < data.length; i++) {
       yeararray[data[i].month] += data[i].money
       total += data[i].money
     }
-    for (i = 1; i < 13; i++) { 
+    for (i = 1; i < 13; i++) {
       yeararray[i].toFixed(2);
-    } 
+    }
     //console.log(yeararray)
     this.assign('yeararray',yeararray);
     total = total.toFixed(2);
     this.assign('total',total);
     return this.display();
   }
-  
+
   async monthAction() {
     var myDate = new Date();
     let year = myDate.getFullYear();
@@ -83,17 +83,17 @@ module.exports = class extends Base {
     //console.log(data)
     let montharray = new Array()
     let i = 0;
-    for (i = 0; i < 32; i++) { 
+    for (i = 0; i < 32; i++) {
       montharray[i] = 0
-    } 
+    }
     let total = 0;
-    for (i = 0; i < data.length; i++) { 
+    for (i = 0; i < data.length; i++) {
       montharray[data[i].day] += data[i].money
       total += data[i].money
     }
-    for (i = 1; i < 13; i++) { 
+    for (i = 1; i < 13; i++) {
       montharray[i].toFixed(2);
-    } 
+    }
     total = total.toFixed(2);
     this.assign('montharray',montharray);
     this.assign('total',total);
@@ -108,14 +108,14 @@ module.exports = class extends Base {
     this.assign('month',month);
     let day = myDate.getDate();
     this.assign('day',day);
-    
+
     let date = this.get();
     const Count_money = this.model('Count_money');
     const data = await Count_money.where({year: date.year, month: date.month, day: date.day}).select();
     this.assign('data',data);
     let total = 0;
     let i = 0;
-    for (i = 0; i < data.length; i++) { 
+    for (i = 0; i < data.length; i++) {
       total += data[i].money
     }
     total = total.toFixed(2);
@@ -134,10 +134,27 @@ module.exports = class extends Base {
     }else{
       this.json({"succ":false});
     }
-    
+
   }
 
   async jsAction() {
     return this.display();
+  }
+
+  async downAction() {
+    let data = this.post();
+    //console.log(data)
+    if (data.isprompt == "qwe123"){
+      const Count_money = this.model('Count_money');
+      let down = await Count_money.where({year: data.year}).select();
+      this.json({
+        "succ":true,
+        down
+      })
+    }else{
+      this.json({
+        "succ":false
+      });
+    }
   }
 };
