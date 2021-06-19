d5936a1cdb576d07271a8cc890ca2dcb1c658252
likangjian <1480108535@qq.com>
2021-06-19 19:25:49 +0800

    修改账单小数点保留2位的bug

--- src/controller/show.js
+++ src/controller/show.js
@@ -25,9 +25,8 @@ module.exports = class extends Base {
       total += data[i].money
     }
     for (i = 2021; i < 2030; i++) {
-      allarray[i].toFixed(2);
+      allarray[i] = allarray[i].toFixed(2);
     }
-    //console.log(yeararray)
     this.assign('allarray',allarray);
     total = total.toFixed(2);
     this.assign('total',total);
@@ -58,9 +57,9 @@ module.exports = class extends Base {
       total += data[i].money
     }
     for (i = 1; i < 13; i++) {
-      yeararray[i].toFixed(2);
+      yeararray[i] = yeararray[i].toFixed(2);
     }
-    //console.log(yeararray)
+
     this.assign('yeararray',yeararray);
     total = total.toFixed(2);
     this.assign('total',total);
@@ -84,18 +83,19 @@ module.exports = class extends Base {
     let montharray = new Array()
     let i = 0;
     for (i = 0; i < 32; i++) {
-      montharray[i] = 0
+      montharray[i] = 0.
     }
     let total = 0;
     for (i = 0; i < data.length; i++) {
       montharray[data[i].day] += data[i].money
       total += data[i].money
     }
-    for (i = 1; i < 13; i++) {
-      montharray[i].toFixed(2);
+    for (i = 1; i < montharray.length; i++) {
+      montharray[i] = montharray[i].toFixed(2);
     }
     total = total.toFixed(2);
     this.assign('montharray',montharray);
+    //console.log(montharray)
     this.assign('total',total);
     return this.display();
   }
@@ -112,6 +112,7 @@ module.exports = class extends Base {
     let date = this.get();
     const Count_money = this.model('Count_money');
     const data = await Count_money.where({year: date.year, month: date.month, day: date.day}).select();
+
     this.assign('data',data);
     let total = 0;
     let i = 0;
@@ -125,7 +126,7 @@ module.exports = class extends Base {
 
   async deleteAction() {
     let data = this.post();
-    //console.log(data)
+    console.log(data)
     const Count_money = this.model('Count_money');
     //console.log(data.isprompt == "lkjwyc123")
     if (data.isprompt == "qwe123"){
@@ -137,6 +138,7 @@ module.exports = class extends Base {
 
   }
 
+
   async jsAction() {
     return this.display();
   }
@@ -146,7 +148,7 @@ module.exports = class extends Base {
     //console.log(data)
     if (data.isprompt == "qwe123"){
       const Count_money = this.model('Count_money');
-      let down = await Count_money.where({year: data.year, month: data.month}).select();
+      let down = await Count_money.where({year: data.year}).select();
       this.json({
         "succ":true,
         down
@@ -157,4 +159,5 @@ module.exports = class extends Base {
       });
     }
   }
+
 };
